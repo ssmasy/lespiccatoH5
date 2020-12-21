40f734d60fa0417b01abc5626a7983c7f80bdcd2
MimoriTyann <38640496+MimoriTyann@users.noreply.github.com>
2020-12-21 15:26:03 +0800

    h5code4

---
+++ src/components/contact.vue
@@ -0,0 +1,127 @@
+<template>
+  <div>
+    <top></top>
+    <div class="banner">
+      <img src="./contact/banner.png" />
+    </div>
+
+    <div class="info-a">
+      <img class="flower-a" src="./index/flower.png" />
+      <div class="title title-first">LE SPICCATO MUSIC PTE LTD</div>
+      <div class="title-sec">
+        Contact us now to begin your music journey today!
+      </div>
+    </div>
+
+    <div class="contact-list">
+      <img src="./contact/address.png" />
+      <div class="title">address</div>
+      <div class="text">
+        1 Jln Anak Bukit, #02-01 Bukit Timah Plaza,<br />
+        Singapore, 588996
+      </div>
+      <img src="./contact/phone.png" />
+      <div class="title">phone</div>
+      <div class="text">(65) 1234 5678</div>
+      <img src="./contact/email.png" />
+      <div class="title">email</div>
+      <div class="text">service@lespiccato.com.sg</div>
+      <img src="./contact/hours.png" />
+      <div class="title">Business Hours</div>
+      <div class="text hours">
+        Monday – Friday： 10am-9pm<br />
+        Saturday – Sunday：9am-8pm
+      </div>
+    </div>
+    <img class="contact-map" src="./contact/map.png" />
+    <bottom></bottom>
+    <free></free>
+  </div>
+</template>
+
+<script>
+import top from "./common/top/top";
+import bottom from "./common/bottom/bottom";
+import free from "./common/free/free";
+
+export default {
+  name: "contact",
+  components: {
+    top,
+    bottom,
+    free,
+  },
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {},
+  destroyed() {},
+};
+</script>
+
+<style scoped lang="scss">
+.banner {
+  position: relative;
+  width: 100%;
+  img {
+    display: block;
+    width: 100%;
+  }
+}
+
+.info-a {
+  padding: 30px 20px 30px;
+  text-align: center;
+  .flower-a {
+    display: block;
+    width: 191px;
+    margin: 0 auto 10px;
+  }
+  .title {
+    color: #c5a47e;
+    font-family: "BigCaslon";
+    font-size: 16px;
+    line-height: 21px;
+    margin-bottom: 5px;
+    text-transform: uppercase;
+  }
+  .title-sec {
+    color: #c5a47e;
+    font-family: "Metropolis";
+    font-size: 12px;
+    line-height: 14px;
+  }
+}
+.contact-list {
+  text-align: center;
+  padding-bottom: 10px;
+  img {
+    display: block;
+    margin: 0 auto 10px;
+    width: 32px;
+  }
+  .title {
+    color: #c5a47e;
+    font-family: "Metropolis";
+    text-transform: uppercase;
+    font-size: 12px;
+    line-height: 14px;
+    margin-bottom: 10px;
+  }
+  .text {
+    color: #333;
+    font-family: "Metropolis";
+    font-size: 13px;
+    line-height: 13px;
+    margin-bottom: 30px;
+    &.hours {
+      font-size: 10px;
+    }
+  }
+}
+.contact-map {
+  display: block;
+  width: 100%;
+}
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,233 @@
+<template>
+  <div>
+    <top></top>
+    <div class="banner">
+      <img src="./index/banner.png" />
+      <a target="_blank" href="https://lespiccato.setmore.com"></a>
+    </div>
+    <div class="info-a">
+      <img class="flower-a" src="./index/flower.png" />
+      <div class="title">MUSIC MUST ALWAYS MOVE <br />FORWARD</div>
+      <div class="cont">
+        Music is a form of art and entertainment that is continuously evolving
+        throughout time. It is constantly invented and shared worldwide by
+        talented individuals. At Le Spiccato, we will help to shape you into
+        your own masterpiece.
+      </div>
+      <img class="flower-b" src="./index/flower.png" />
+      <div class="title">MUSIC COURSES</div>
+
+      <div class="cont">
+        Le Spiccato provides specialised and tailored lessons from our qualified
+        music educators, including instrumental training, improvising
+        coaching,early-care music enrichment courses and more.
+      </div>
+
+      <div class="banner-courses">
+        <div class="banner-courses-top">
+          <router-link :to="{ path: '/courses' }" class="courses-wrap">
+            <img class="courses-pic" src="./index/violin.png" />
+          </router-link>
+          <router-link :to="{ path: '/courses' }" class="courses-wrap">
+            <img class="courses-pic" src="./index/piano.png" />
+          </router-link>
+        </div>
+        <div class="banner-courses-bottom">
+          <router-link :to="{ path: '/courses' }" class="courses-wrap">
+            <img class="courses-pic" src="./index/cello.png" />
+          </router-link>
+          <router-link :to="{ path: '/courses' }" class="courses-wrap">
+            <img class="courses-pic" src="./index/vocal.png" />
+          </router-link>
+          <router-link :to="{ path: '/courses' }" class="courses-wrap">
+            <img class="courses-pic" src="./index/guitar.png" />
+          </router-link>
+        </div>
+      </div>
+
+      <div class="button-wrap">
+        <router-link :to="{ path: '/courses' }" class="button">
+          <div class="button-text">VIEW FULL COURSES</div>
+        </router-link>
+      </div>
+    </div>
+    <img class="student" src="./index/student.png" />
+    <div class="info-b">
+      <div class="title">STUDENT LIFE</div>
+      <div class="cut"></div>
+      <div class="cont">
+        Not only will the students learn to master different instruments and
+        skills, we are keen in adding your characteristics into the songs you
+        wish to learn. Helping you reach your goals and race towards success in
+        no time. Different events will also be held in our concert hall for the
+        students to showcase their skills and knowledge.
+      </div>
+    </div>
+    <div class="bottom-banner">
+      <img src="./index/bottom-banner.png" />
+      <a target="_blank" href="https://lespiccato.setmore.com"></a>
+    </div>
+    <bottom></bottom>
+    <free></free>
+  </div>
+</template>
+
+<script>
+import top from "./common/top/top";
+import bottom from "./common/bottom/bottom";
+import free from "./common/free/free";
+
+export default {
+  name: "index",
+  components: {
+    top,
+    bottom,
+    free,
+  },
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {},
+  destroyed() {},
+};
+</script>
+
+<style scoped lang="scss">
+.banner {
+  position: relative;
+  width: 100%;
+  img {
+    display: block;
+    width: 100%;
+  }
+  a {
+    display: block;
+    position: absolute;
+    width: 44%;
+    height: 14%;
+    top: 68%;
+    left: 28%;
+  }
+}
+.info-a {
+  padding: 30px 20px 0;
+  text-align: center;
+  .flower-a {
+    display: block;
+    width: 191px;
+    margin: 0 auto 10px;
+  }
+  .flower-b {
+    display: block;
+    width: 191px;
+    margin: 40px auto 10px;
+  }
+  .title {
+    color: #c5a47e;
+    font-family: "BigCaslon";
+    font-size: 16px;
+    line-height: 21px;
+    margin-bottom: 10px;
+  }
+  .cont {
+    color: #333333;
+    font-family: "Metropolis";
+    font-size: 13px;
+    line-height: 17px;
+  }
+
+  .banner-courses {
+    max-width: 1200px;
+    margin: 0 auto;
+    .courses-wrap {
+      position: relative;
+      display: block;
+      .courses-pic {
+        display: block;
+        width: 100%;
+      }
+    }
+
+    .banner-courses-top {
+      display: flex;
+      margin-left: -3px;
+      margin-bottom: 3px;
+      margin-top: 20px;
+      .courses-wrap {
+        width: 50%;
+        margin-left: 3px;
+      }
+    }
+    .banner-courses-bottom {
+      display: flex;
+      margin-left: -3px;
+      margin-bottom: 20px;
+      .courses-wrap {
+        width: 33.33%;
+        margin-left: 3px;
+      }
+    }
+  }
+
+  .button-wrap {
+    text-align: center;
+    margin-bottom: 40px;
+    .button {
+      display: inline-block;
+      background-color: #c5a47e;
+      border-radius: 38px;
+      .button-text {
+        display: inline-block;
+        padding: 8px 38px;
+        font-size: 12px;
+        font-family: "DIN";
+        color: #fefefe;
+        line-height: 14px;
+        letter-spacing: 2px;
+      }
+    }
+  }
+}
+.student {
+  display: block;
+  width: 100%;
+}
+.info-b {
+  padding: 20px 20px 40px;
+  .title {
+    color: #c5a47e;
+    font-family: "BigCaslon";
+    font-size: 16px;
+    line-height: 21px;
+  }
+  .cut {
+    width: 30px;
+    height: 4px;
+    background-color: #c5a47e;
+    margin: 10px 0 20px;
+  }
+  .cont {
+    color: #333333;
+    font-family: "Metropolis";
+    font-size: 13px;
+    line-height: 17px;
+  }
+}
+.bottom-banner {
+  position: relative;
+  width: 100%;
+  img {
+    display: block;
+    width: 100%;
+  }
+  a {
+    display: block;
+    position: absolute;
+    width: 74%;
+    height: 15%;
+    top: 71%;
+    left: 13%;
+  }
+}
+</style>
